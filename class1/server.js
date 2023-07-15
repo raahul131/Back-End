42a6378584bdc9da7797039609c61968e05e5745
Rahul R Yadav <rryadav2025@gmail.com>
2023-07-15 18:40:50 +0530

    first commit

---
+++ class1/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 5001;
+app.listen(PORT, () => {
+  console.log("Server running at port number:" + PORT);
+});
+
+
+app.get
